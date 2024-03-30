c2ae7f4f91e41992b810f9456ef3f68b12939843
Timothy Zhou <Timothy_Zhou@hotmail.com>
2024-03-30 10:54:57 -0700

    Imported dataset, checked for null, started eda

--- eda.R
+++ eda.R
@@ -1,1 +1,16 @@
-# Exploratory data analysis
+# Import Dataset
+df = read.csv("avocado.csv", header = TRUE)
+drops <- c("X") # Dropped unnecessary column
+df = df[ , !(names(df) %in% drops)]
+na_count = sum(is.na(df))
+if(na_count==0){
+  print("The dataset contains no na value.")
+} else{
+  sprintf("The dataset contains %d na values", na_count)
+}
+head(df)
+
+# EDA
+# import ggplot library
+install.packages("tidyverse")
+library(ggplot2)
