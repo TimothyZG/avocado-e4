1b960da6cee54f42d3efddec3aa55d2fc2371b2c
Timothy Zhou <Timothy_Zhou@hotmail.com>
2024-03-30 13:51:12 -0700

    added date to month feature transformation and first eda plot

--- eda.R
+++ eda.R
@@ -1,6 +1,9 @@
 # Import Dataset
 df = read.csv("avocado.csv", header = TRUE)
-drops <- c("X") # Dropped unnecessary column
+
+# Dropped unnecessary column
+drops <- c("X","X4046","X4225","X4770","Total.Bags",
+           "Small.Bags","Large.Bags", "XLarge.Bags")
 df = df[ , !(names(df) %in% drops)]
 na_count = sum(is.na(df))
 if(na_count==0){
@@ -8,9 +11,26 @@ if(na_count==0){
 } else{
   sprintf("The dataset contains %d na values", na_count)
 }
+df$type = factor(df$type)
+df$region = factor(df$region)
+df$year = factor(df$year)
+
+# Feature transformation (date -> month) 
+install.packages("lubridate")
+library(lubridate)
+df$Date <- ymd(df$Date)
+# Extract the month number
+df$Month <- factor(month(df$Date))
+
+#Data Wrangling and Transformation done, here's a preview of cleaned data:
 head(df)
 
 # EDA
 # import ggplot library
 install.packages("tidyverse")
 library(ggplot2)
+ggplot(df, aes(AveragePrice, Total.Volume, colour = type)) + 
+  geom_point()+
+  ggtitle("Volume against price and type")+
+  xlab("Average Price")+
+  ylab("Total Volume Sold")
