3c99e08f6002ae282ee7bec35216c8ab31c98dff
Roy <roy.rtao@gmail.com>
2024-04-02 17:08:10 -0700

    Added some analysis to eda, including code to filter the dataframe, removing entries that include totals for regions of the US

--- eda.R
+++ eda.R
@@ -34,3 +34,56 @@ ggplot(df, aes(AveragePrice, Total.Volume, colour = type)) +
   ggtitle("Volume against price and type")+
   xlab("Average Price")+
   ylab("Total Volume Sold")
+
+ggplot(df, aes(year, Total.Volume, colour = type)) +
+  geom_point()+
+  ggtitle("Volume against year and type")+
+  xlab("Year sold")+
+  ylab("Total Volume Sold")
+
+# From these two plots, we see that there is a great difference between the
+# volume of conventional and organic avocados sold, as such, I believe that it 
+# would make sense to analyze both individually
+
+ggplot(subset(df, type == "conventional"), aes(year, Total.Volume)) +
+  geom_point() +
+  ggtitle("Volume against year for Type_A") +
+  xlab("Year sold") +
+  ylab("Total Volume Sold")
+
+ggplot(subset(df, type == "organic"), aes(year, Total.Volume)) +
+  geom_point() +
+  ggtitle("Volume against year for Type_A") +
+  xlab("Year sold") +
+  ylab("Total Volume Sold")
+
+# In both the conventional and the organic avocado plots, it can be seen that
+# there are two distinct groups, to see why there's this difference, we will
+# group by the other variables to see if we can identify the reason
+
+# Inspecting the data more closely, it can be seen that many of the top results 
+# are for total US/ regions of the US, aka west, east, and central. In order to
+# account for this, we can remove these points and analyze them separately
+
+regions_to_exclude <- c("TotalUS", "West", "SouthCentral", "SouthEast", 
+                        "Midsouth", "GreatLakes", "Southeast", "Northeast",
+                        "Plains")
+
+# Filter the dataframe to exclude the specified regions
+df_filtered <- subset(df, !(region %in% regions_to_exclude))
+
+# Once removing these results, the results become more consistent
+
+ggplot(subset(df_filtered, type == "organic"), aes(year, Total.Volume, colour = season)) +
+  geom_point() +
+  ggtitle("Volume against year for filtered (organic)") +
+  xlab("Year sold") +
+  ylab("Total Volume Sold")
+
+ggplot(subset(df_filtered, type == "conventional"), aes(year, Total.Volume)) +
+  geom_point() +
+  ggtitle("Volume against year for filtered (conventional") +
+  xlab("Year sold") +
+  ylab("Total Volume Sold")
+
+df_totals <- subset(df, (region %in% regions_to_exclude))
